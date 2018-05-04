3b0ce137064d809b3c605f34e9ec34180a0b9ff2
xiayesuifeng <1377195627@qq.com>
2018-05-05 01:23:28 +0800

    add setLocale and setHomeName function

--- core/utils.go
+++ core/utils.go
@@ -1,6 +1,9 @@
 package core
 
-import "os/exec"
+import (
+	"os/exec"
+	"io/ioutil"
+)
 
 func UserAdd(username,password string) error {
 	cmd := exec.Command("useradd","-m","-g","user","-G","wheel",username)
@@ -19,4 +22,18 @@ func UserAdd(username,password string) error {
 	}
 
 	return nil
+}
+
+func SetHomeName(hostname string) error {
+	return ioutil.WriteFile("/etc/hostname",[]byte(hostname),0644)
+}
+
+func SetLocale(username string) error {
+	profile := `#!/bin/bash
+export LANG=zh_CN.UTF-8
+export LANGUAGE=zh_CN:en_US
+export LC_CTYPE=zh_CN.UTF-8
+`
+
+	return ioutil.WriteFile("/home/"+username+"/.xprofile",[]byte(profile),0644)
 }
