90a852f69c9e38b6db3690c4a46fdc85ab014b2e
xiayesuifeng <1377195627@qq.com>
2018-05-05 20:56:48 +0800

    add setTips function to EndPage

--- ui/page/endPage.go
+++ ui/page/endPage.go
@@ -7,10 +7,12 @@ import (
 
 type EndPage struct {
 	*widgets.QFrame
+
+	tipsLabel *widgets.QLabel
 }
 
 func NewEndPage(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *EndPage {
-	page := &EndPage{widgets.NewQFrame(parent, fo)}
+	page := &EndPage{QFrame:widgets.NewQFrame(parent, fo)}
 
 	page.init()
 
@@ -21,11 +23,15 @@ func (page *EndPage) init() {
 	vboxLayout := widgets.NewQVBoxLayout2(page)
 	vboxLayout.SetContentsMargins(0, 0, 0, 0)
 
-	label := widgets.NewQLabel2("请稍等...",page,0)
+	page.tipsLabel = widgets.NewQLabel2("请稍等...",page,0)
 
 	vboxLayout.AddStretch(1)
-	vboxLayout.AddWidget(label,0,core.Qt__AlignCenter)
+	vboxLayout.AddWidget(page.tipsLabel,0,core.Qt__AlignCenter)
 	vboxLayout.AddStretch(1)
 
 	page.SetLayout(vboxLayout)
+}
+
+func (page *EndPage) SetTips(tips string) {
+	page.tipsLabel.SetText(tips)
 }
